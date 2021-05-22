7d36669640afa2f630d475f0fcbf7ccb49347091
fuhui <744245151@qq.com>
2021-05-23 02:08:16 +0800

    add user create and auth interface

--- src/main.go
+++ src/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/fu-hui/am/src/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	userGroup := r.Group("/am/v1/user")
+
+	// create user
+	userGroup.POST("", controller.CreateUser)
+
+	// user auth v1
+	userGroup.POST("/auth/v1", controller.UserAuthV1)
+
+	// service heartbeat test
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	// listen and serve on 0.0.0.0:8080
+	_ = r.Run()
 }
